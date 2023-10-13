4cc8bfdb765580ab86997c451d1606c19a397656
Egor Volkov <egor.volkov20038@gmail.com>
2023-10-13 19:17:40 +0300

    minor

--- website/MapApp/static/js/map.js
+++ website/MapApp/static/js/map.js
@@ -1,6 +1,23 @@
 ymaps.ready(function () {
-    var myMap = new ymaps.Map("YMapsID", {
-      center: [55.76, 37.64],
-      zoom: 10
+    var map;
+    ymaps.geolocation.get().then(function (res) {
+        var mapContainer = $('#map'),
+            bounds = res.geoObjects.get(0).properties.get('boundedBy'),
+            // Рассчитываем видимую область для текущей положения пользователя.
+            mapState = ymaps.util.bounds.getCenterAndZoom(
+                bounds,
+                [mapContainer.width(), mapContainer.height()]
+            );
+        createMap(mapState);
+    }, function (e) {
+        // Если местоположение невозможно получить, то просто создаем карту.
+        createMap({
+            center: [55.751574, 37.573856],
+            zoom: 2
+        });
     });
-  });
+
+    function createMap (state) {
+        map = new ymaps.Map('map', state);
+    }
+});
